908f30f41f65a222a2e92e8319b9f5f90bd495b2
Rahul shah <44189476+shah-rahul@users.noreply.github.com>
2021-11-08 22:37:51 +0530

    [feature] export to xlsx

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,10 +1,62 @@
 import React from "react";
 import Navbar from "../navbar/Navbar,";
-
+import XLSX from "xlsx";
+const colorData = [
+  {
+    color: "red",
+    value: "#f00",
+  },
+  {
+    color: "green",
+    value: "#0f0",
+  },
+  {
+    color: "blue",
+    value: "#00f",
+  },
+  {
+    color: "cyan",
+    value: "#0ff",
+  },
+  {
+    color: "magenta",
+    value: "#f0f",
+  },
+  {
+    color: "yellow",
+    value: "#ff0",
+  },
+  {
+    color: "black",
+    value: "#000",
+  },
+];
 const Dashboard = () => {
+  const downloadExcel = () => {
+    const newData = colorData.map((row) => {
+      delete row.tableData;
+      return row;
+    });
+    const workSheet = XLSX.utils.json_to_sheet(newData);
+    const workBook = XLSX.utils.book_new();
+
+    XLSX.utils.book_append_sheet(
+      workBook,
+      workSheet,
+      "student attendance record"
+    );
+
+    //buffer
+    let buffer = XLSX.write(workBook, { bookType: "xlsx", type: "buffer" });
+    //binary string
+    XLSX.write(workBook, { bookType: "xlsx", type: "binary" });
+    //download
+    XLSX.writeFile(workBook, "AttendanceData.xlsx");
+  };
   return (
     <div>
       <Navbar></Navbar>
+
       <div className="select_tags">
         {/* ------------------------Course selector ----------------- */}
 
@@ -19,6 +71,7 @@ const Dashboard = () => {
         <select name="Subject Name" id="subject">
           <option>Select Date</option>
         </select>
+        <button onClick={downloadExcel}>click to export</button>
       </div>
       <div className="table-container">
         <table>
