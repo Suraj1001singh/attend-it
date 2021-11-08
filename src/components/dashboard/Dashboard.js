95630d722982b0747b355feca33e599687eef3f0
Rahul shah <44189476+shah-rahul@users.noreply.github.com>
2021-11-08 21:48:00 +0530

    [UI UPDATE] - dashboard UI

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -5,7 +5,45 @@ const Dashboard = () => {
   return (
     <div>
       <Navbar></Navbar>
-      <h1>THIS IS DASHBOARD</h1>
+      <div className="select_tags">
+        {/* ------------------------Course selector ----------------- */}
+
+        <select name="Course Name" id="course">
+          <option>Select Course</option>
+        </select>
+        {/* ------------------------Subject selector ----------------- */}
+
+        <select name="Subject Name" id="subject">
+          <option>Select Subject</option>
+        </select>
+        <select name="Subject Name" id="subject">
+          <option>Select Date</option>
+        </select>
+      </div>
+      <div className="table-container">
+        <table>
+          <thead>
+            <tr>
+              <th>Roll Number</th>
+              <th>Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>21413412353</td>
+              <td>suraj mitr</td>
+            </tr>
+            <tr>
+              <td>21413412353</td>
+              <td>suraj mitr</td>
+            </tr>
+            <tr>
+              <td>21413412353</td>
+              <td>suraj mitr</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
