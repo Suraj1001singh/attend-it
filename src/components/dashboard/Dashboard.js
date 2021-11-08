a124b63189e0be4993521d6f12e75199f7eea874
Suraj Singh <absurajsinghchamiyal@gmail,com>
2021-11-09 01:35:24 +0530

    pulling code

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import Navbar from "../navbar/Navbar,";
 import db from "../../firebase-config";
-import { collection, getDocs, addDoc, query, where } from "@firebase/firestore";
+import { collection, getDocs, addDoc, query, where, firebase, collectionGroup } from "@firebase/firestore";
 
 const Dashboard = () => {
   //this will be fetched from api
   const [courseList, setCourseList] = useState([]);
   const [subjectList, setSubjectList] = useState([]);
+  const [dateList, setDateList] = useState([]);
 
   const [selectedCourse, setSelectedCourse] = useState({});
   const [selectedSubject, setSelectedSubject] = useState({});
@@ -17,9 +18,17 @@ const Dashboard = () => {
 
   const getStudents = async () => {
     if (Object.keys(selectedCourse).length != 0 && Object.keys(selectedSubject).length != 0 && selectedDate !== "") {
-      const studentCollectionRef = collection(db, `courses/${selectedCourse.id}/subject/${selectedSubject.id}/${selectedSubject.name}`);
+      const studentCollectionRef = query(collectionGroup(db, "arrayofdates"), where("subjectname", "==", selectedSubject.name));
+      // const newred=collection(studentCollectionRef,selectedSubject.id);
+      // const newred2=collection(newred,"cs2021-11-8");
+      // const studentCollectionRef = await firebase.firestore().collection("events").get();
+      // var data = db.collection("courses").doc(selectedCourse.id);
       const data = await getDocs(studentCollectionRef);
-      console.log(data);
+      // const dataArray = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+
+      setDateList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      // console.log(`courses/${selectedCourse.id}/subject/${selectedSubject.id}/arrayofdates}`);
+      // var docRef = db.collection("courses").doc(selectedCourse.id);
     } else {
       alert("Please select All fields");
     }
@@ -100,7 +109,14 @@ const Dashboard = () => {
               ))}
             </select>
             {/* --------------------------Date selector------------ */}
-            <input type="date" onChange={(e) => handelOnchangeDate(e)}></input>
+            <select name="Subject Name" id="subject" onChange={(e) => handelOnChangeSubject(e)}>
+              <option>Select Date</option>
+              {dateList.map((date) => (
+                <option key={date.id} value={date.id}>
+                  {date.date}
+                </option>
+              ))}
+            </select>
           </div>
           <div className="button_div">
             <button className="button" onClick={getStudents}>
